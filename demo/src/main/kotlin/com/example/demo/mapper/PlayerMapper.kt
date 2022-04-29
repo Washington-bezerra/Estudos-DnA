237da8c7f76a635c08c695ffb988d164632cf038
washington-bezerra <luiswashington00@gmail.com>
2022-04-28 21:11:46 -0300

    added validações

--- demo/src/main/kotlin/com/example/demo/mapper/PlayerMapper.kt
+++ demo/src/main/kotlin/com/example/demo/mapper/PlayerMapper.kt
@@ -2,9 +2,11 @@ package com.example.demo.mapper
 
 import com.example.demo.domain.CreatePlayerRequest
 import com.example.demo.entities.Player
+import com.example.demo.exception.AlreadyRegistered
 import com.example.demo.repositories.CountryRepository
 import org.springframework.beans.factory.annotation.Autowired
 import com.example.demo.exception.NotFoundException
+import com.example.demo.repositories.PlayerRepository
 import org.springframework.stereotype.Service
 import java.util.Optional
 
@@ -13,17 +15,25 @@ import java.util.Optional
 class PlayerMapper:Mapper<CreatePlayerRequest, Player>{
     @Autowired
     lateinit var repository: CountryRepository
+    @Autowired
+    lateinit var playerrepository: PlayerRepository
 
     override fun map(t: CreatePlayerRequest): Player{
 
-        val test = repository.findByName(t.country).orElseThrow{NotFoundException("country not found")}
-//        val test = repository.findByName(t.country) ?: throw NotFoundException("Country not found")
+        val countryName = repository.findByName(t.country).orElseThrow{NotFoundException("country not found - " +
+                "create a country in http://localhost:8080/country")}
+        val playerNotIsRegistered = playerrepository.findByName(t.name).isEmpty
 
-        return Player(
+        return if(playerNotIsRegistered){
+            Player(
             name = t.name,
             club = t.club,
-            country = repository.findByName(t.country)
-        )
+            country = countryName
+            )
+        }else{
+            return throw AlreadyRegistered("The player is already registered")
+        }
     }
+
 }
 
--- demo/src/main/kotlin/com/example/demo/repositories/PlayerRepository.kt
+++ demo/src/main/kotlin/com/example/demo/repositories/PlayerRepository.kt
@@ -2,9 +2,10 @@ package com.example.demo.repositories
 
 import com.example.demo.entities.Player
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface PlayerRepository: JpaRepository<Player, Long> {
+    fun findByName(name: String): Optional<Player>
 }

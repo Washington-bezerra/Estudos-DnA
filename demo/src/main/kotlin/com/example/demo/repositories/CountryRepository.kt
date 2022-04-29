1a89792fa24eb9a3a5608ac6f9b962324bbfa789
washington-bezerra <luiswashington00@gmail.com>
2022-04-28 21:09:37 -0300

    corretão para usar o OrElseThrow

--- demo/src/main/kotlin/com/example/demo/repositories/CountryRepository.kt
+++ demo/src/main/kotlin/com/example/demo/repositories/CountryRepository.kt
@@ -4,10 +4,11 @@ import com.example.demo.entities.Country
 import com.example.demo.entities.Player
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface CountryRepository: JpaRepository<Country, Long>{
 
-    fun findByName(name: String):Country
+    fun findByName(name: String): Optional<Country>
 
 }
